942d6e2b900104410aa727b0a41b028012427396
MRJAHID <mrjahid350@gmail.com>
2021-06-15 17:08:13 +0600

    Header and hero section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            rubik: ['Rubik', 'sans-serif'],
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
